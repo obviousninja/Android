d09b67edcfc7e58d4f37fdf5f7a4afcbabb935a3
Jing <infinityarc@gmail.com>
2015-09-30 14:07:00 -0400

    initial driver interface

--- In-Tune-Driver/app/src/main/java/intunex3/intunedriverinterface/DriverLoginActivity.java
+++ In-Tune-Driver/app/src/main/java/intunex3/intunedriverinterface/DriverLoginActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class DriverLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.driver_Login);
+
+
     }
 
     @Override
